7f34a5fe12b77899808ab9ca1f5043ba407f0702
sugurunatsuno <suguru.irie@gmail.com>
2018-11-14 23:21:15 +0900

    1st commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("login")
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("/neko"):
+        reply = "にゃーん"
+        print(reply)
+        await client.send_message(message.channel, reply)
+
+
+@client.event
+async def on_voice_state_update(before, after):
+    avc = None
+    if after.voice.voice_channel is not None:
+        avc = after.voice.voice_channel
+
+    if avc is not None:
+        ch = [channel for channel in before.server.channels if channel.name == "general"][0]
+        await client.send_message(ch, "ドーモ、{}サン".format(after.name))
+
+
+token = os.environ["LOL"]
+
+client.run(token)
